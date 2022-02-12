3cdd26d9ebea5020f294a90880065c73cbcb835b
AminNasirov <amin_nasirov@yahoo.com>
2022-02-12 17:41:22 -0500

    Java programing 02.12.2022 (1)

--- src/day20_Arrays/CommonElements.java
+++ src/day20_Arrays/CommonElements.java
@@ -6,9 +6,9 @@ public class CommonElements {
        int[] arr1= {1,2,3,4,5};
         int[] arr2= {4,5,6,7,8};
 
-          int num =0;
-        for (int i = 0; i < arr1.length; i++) {
 
+        for (int i = 0; i < arr1.length; i++) {
+            int num =0;
             for (int j = 0; j < arr2.length; j++) {
                 if(arr1[i]==arr2[j]){
                     num=arr2[j];
--- src/day40_FinalKeyword/day41_Exceptions/TryAndCatchBlocks2.java
+++ src/day40_FinalKeyword/day41_Exceptions/TryAndCatchBlocks2.java
@@ -1,4 +1,4 @@
-package day41_Exceptions;
+package day40_FinalKeyword.day41_Exceptions;
 
 public class TryAndCatchBlocks2 {
     public static void main(String[] args){
