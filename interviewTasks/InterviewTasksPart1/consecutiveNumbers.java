39278b306b90ddacdb7cd3a893460e299c0a2b0e
AminNasirov <amin_nasirov@yahoo.com>
2022-02-12 22:08:10 -0500

    Java programing 02.12.2022 (3)

---
+++ interviewTasks/InterviewTasksPart1/consecutiveNumbers.java
@@ -0,0 +1,34 @@
+package InterviewTasksPart1;
+
+public class consecutiveNumbers {
+    public static void main(String[] args) {
+
+
+        int N = 24;
+
+        String name = "";
+
+        for (int i = 1; i <= N; i++) {
+
+            if (i % 3 == 0 && i % 2 == 0 && i % 5 == 0) {
+                System.out.println("CodilityTestCoders");
+            } else if (i % 2 == 0 && i % 5 == 0) {
+                System.out.println("CodilityCoders");
+            } else if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("TestCoders");
+            } else if (i % 3 == 0 && i % 2 == 0) {
+                System.out.println("CodilityTest");
+            } else if (i % 5 == 0) {
+                System.out.println("Coders");
+            } else if (i % 3 == 0) {
+                System.out.println("Test");
+            } else if (i % 2 == 0) {
+                System.out.println("Codility");
+            } else {
+                System.out.println(i);
+            }
+
+
+        }
+    }
+}
